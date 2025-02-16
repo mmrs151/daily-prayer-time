89b8be6f617fb32104a67dca9b117f6d4a54e055
mmrs151 <mmrs151@gmail.com>
2025-02-16 23:46:10 +0000

    load cities on demand

--- Views/Tabs/CsvUpload.php
+++ Views/Tabs/CsvUpload.php
@@ -1,7 +1,8 @@
 <?php
 use IslamicNetwork\PrayerTimes\Method;
-use IslamicNetwork\PrayerTimes\PrayerTimes;
 
+$loadCities = false;
+$loadCities = !empty($_POST['activate-automatic']);
 $worldCities = new WorldCities();
 $cities = $worldCities->getCities();
 $adapter = new DatabaseConnection();
@@ -39,72 +40,77 @@ if ( isset($rows[1])) {
         <div class="col-sm-6 col-xs-6 automatic">
             <div class="head">
                 <h3 style="padding-bottom: 30px;" xmlns="http://www.w3.org/1999/html">Set Prayer Times Automatically
+                    <input type="submit" name="activate-automatic" class="button button-primary" value="Load Cities" onclick="window.location.href='admin.php?page=dpt'">
                 <sub><p style="color: red;">(Please check and <a class="url" href="admin.php?page=dpt#tabs-2">Quick Update</a> before publishing)</p></sub></h3>
             </div>
             <div class="upload-step">
                 <label>Select your nearest city:</label>
                     <select class="form-select" data-live-search="true" name="city">
                         <option></option>
-                        <?php                    
-//                        foreach ($cities as $city) {
-//                            $selected = $city['id'] == get_transient('nearest_city') ? "selected" : null;
-//                            echo "<option
-//                                class='auto-settings'
-//                                value=" . esc_attr($city['id']) . "  ". $selected.">" .
-//                                esc_html($city["country"]) . ", " . esc_html($city['city']) . " (Lat:" . esc_html($city['lat']) .")
-//                                </option>";
-//                        }
+                        <?php
+                        if ($loadCities) {
+                            foreach ($cities as $city) {
+                                $selected = $city['id'] == get_transient('nearest_city') ? "selected" : null;
+                                echo "<option
+                                class='auto-settings'
+                                value=" . esc_attr($city['id']) . "  ". $selected.">" .
+                                    esc_html($city["country"]) . ", " . esc_html($city['city']) . " (Lat:" . esc_html($city['lat']) .")
+                                </option>";
+                            }
+                        }
                         ?>
                     </select>
             </div>
-            <div class="upload-step">
-                <label>Select calculation method:</label>
-                <select class="form-select" name="method" id="calculationMethod">
-                    <?php
-                    foreach (Method::getMethodCodes() as $key => $method) {
-                        $selected = get_option('calc-method') == $key ? "selected" : null;
-                        echo "<option value=" . $key . " " . $selected . ">" . $method . "</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="upload-step" id="customMethod" style="background: #f1f1f1; display: none;">
-                <input type="number" value="<?php echo esc_html( get_option('fajr-angle') )?>" placeholder="Fajr angle" name="fajr-angle" style="margin: 20px 0px 0px 30px">
-                <input type="number" value="<?php echo esc_html( get_option('isha-angle') )?>" placeholder="Isha angle" name="isha-angle">
-            </div>
-            <div class="upload-step">
-                <label>Select Asr juristic method:</label>
-                <select class="form-select" name="asr-method">
-                    <option value="0" <?php if (get_option('asr-method') == 0){echo "selected='selected'";}?>>Standard</option>
-                    <option value="1" <?php if (get_option('asr-method') == 1){echo "selected='selected'";}?>>Hanafi</option>
-                </select>
-            </div>
-            <div class="upload-step">
-                <label>Adjusting Methods for Higher Latitudes:</label>
-                <select class="form-select" name="higher-lat">
-                    <option></option>
-                    <option value="3" <?php if (get_option('higher-lat') == 3){echo "selected='selected'";}?>>Angle Based</option>
-                    <option value="2" <?php if (get_option('higher-lat') == 2){echo "selected='selected'";}?>>One Seventh of the Night</option>
-                    <option value="1" <?php if (get_option('higher-lat') == 1){echo "selected='selected'";}?>>Middle of the Night</option>
-                </select>
-            </div>
-            <div class="upload-step">
-                <label>Fajr Iqamah delay:</label><input type="number" name="fajr-delay" min="1" max="120" value="<?php echo esc_html( get_option('fajr-delay') )?>"> mins
-            </div>
-            <div class="upload-step">
-                <label>Dhuhr Iqamah delay:</label><input type="number" name="zuhr-delay" min="1" max="120" value="<?php echo esc_html( get_option('zuhr-delay') )?>"> mins
-            </div>
-            <div class="upload-step">
-                <label>Asr Iqamah delay:</label><input type="number" name="asr-delay" min="1" max="120" value="<?php echo esc_html( get_option('asr-delay') )?>"> mins
-            </div>
-            <div class="upload-step">
-                <label>Maghrib Iqamah delay:</label><input type="number" name="maghrib-delay" min="1" max="120" value="<?php echo esc_html( get_option('maghrib-delay') )?>"> mins
-            </div>
-            <div class="upload-step">
-                <label>Isha Iqamah delay:</label><input type="number" name="isha-delay" min="1" max="120" value="<?php echo esc_html( get_option('isha-delay') )?>"> mins
-            </div>
-            <div class="upload-step">
-                <input type='submit' name="set-start-time" id="set-start-time" class='button button-primary' value='Set Prayer Time'>
+        <div class="automaticPrayerTime">
+                <div class="upload-step">
+                    <label>Select calculation method:</label>
+                    <select class="form-select" name="method" id="calculationMethod">
+                        <?php
+                        foreach (Method::getMethodCodes() as $key => $method) {
+                            $selected = get_option('calc-method') == $key ? "selected" : null;
+                            echo "<option value=" . $key . " " . $selected . ">" . $method . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="upload-step" id="customMethod" style="background: #f1f1f1; display: none;">
+                    <input type="number" value="<?php echo esc_html( get_option('fajr-angle') )?>" placeholder="Fajr angle" name="fajr-angle" style="margin: 20px 0px 0px 30px">
+                    <input type="number" value="<?php echo esc_html( get_option('isha-angle') )?>" placeholder="Isha angle" name="isha-angle">
+                </div>
+                <div class="upload-step">
+                    <label>Select Asr juristic method:</label>
+                    <select class="form-select" name="asr-method">
+                        <option value="0" <?php if (get_option('asr-method') == 0){echo "selected='selected'";}?>>Standard</option>
+                        <option value="1" <?php if (get_option('asr-method') == 1){echo "selected='selected'";}?>>Hanafi</option>
+                    </select>
+                </div>
+                <div class="upload-step">
+                    <label>Adjusting Methods for Higher Latitudes:</label>
+                    <select class="form-select" name="higher-lat">
+                        <option></option>
+                        <option value="3" <?php if (get_option('higher-lat') == 3){echo "selected='selected'";}?>>Angle Based</option>
+                        <option value="2" <?php if (get_option('higher-lat') == 2){echo "selected='selected'";}?>>One Seventh of the Night</option>
+                        <option value="1" <?php if (get_option('higher-lat') == 1){echo "selected='selected'";}?>>Middle of the Night</option>
+                    </select>
+                </div>
+                <div class="upload-step">
+                    <label>Fajr Iqamah delay:</label><input type="number" name="fajr-delay" min="1" max="120" value="<?php echo esc_html( get_option('fajr-delay') )?>"> mins
+                </div>
+                <div class="upload-step">
+                    <label>Dhuhr Iqamah delay:</label><input type="number" name="zuhr-delay" min="1" max="120" value="<?php echo esc_html( get_option('zuhr-delay') )?>"> mins
+                </div>
+                <div class="upload-step">
+                    <label>Asr Iqamah delay:</label><input type="number" name="asr-delay" min="1" max="120" value="<?php echo esc_html( get_option('asr-delay') )?>"> mins
+                </div>
+                <div class="upload-step">
+                    <label>Maghrib Iqamah delay:</label><input type="number" name="maghrib-delay" min="1" max="120" value="<?php echo esc_html( get_option('maghrib-delay') )?>"> mins
+                </div>
+                <div class="upload-step">
+                    <label>Isha Iqamah delay:</label><input type="number" name="isha-delay" min="1" max="120" value="<?php echo esc_html( get_option('isha-delay') )?>"> mins
+                </div>
+                <div class="upload-step">
+                    <input type='submit' name="set-start-time" id="set-start-time" class='button button-primary' value='Set Prayer Time'>
+                </div>
             </div>
         </div>
         <div class="col-sm-6 col-xs-6 manual" style="background: #eeeeee;">
